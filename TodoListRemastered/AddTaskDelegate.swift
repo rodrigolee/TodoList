e44d07d9002a98f04144ba6b59a57861c72fa48f
Rodrigo Lee <rodrigolee@Rodrigos-MacBook-Pro.local>
2017-05-15 16:57:20 -0700

    hi

---
+++ TodoListRemastered/AddTaskDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AddTaskDelegate.swift
+//  TodoListRemastered
+//
+//  Created by Rodrigo Lee on 4/18/17.
+//  Copyright © 2017 Rodrigo Lee. All rights reserved.
+//
+
+import Foundation
---
+++ TodoListRemastered/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  TodoListRemastered
+//
+//  Created by Rodrigo Lee on 4/18/17.
+//  Copyright © 2017 Rodrigo Lee. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
